7e8bff8dae3a803cccc997123496fb1f9a42a421
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-13 16:28:00 +0700

    Console log when a new user is added or deleted

--- updateOneSignalUsers/src/main.js
+++ updateOneSignalUsers/src/main.js
@@ -1,33 +1,27 @@
 import { Client } from 'node-appwrite';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
+  // Log messages to the console
   log('Hello, Logs!');
-
-  // If something goes wrong, log an error
   error('Hello, Errors!');
 
   // The `req` object contains the request data
   if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+      return res.send('Hello, World!');
+  }
+
+  // Check if the event type is a user-related event
+  if (req.body.event && req.body.event === 'users.create') {
+      console.log('User created:', req.body.payload.$id);
+  } else if (req.body.event && req.body.event === 'users.delete') {
+      console.log('User deleted:', req.body.payload.$id);
   }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+      motto: 'Build like a team of hundreds_',
+      learn: 'https://appwrite.io/docs',
+      connect: 'https://appwrite.io/discord',
+      getInspired: 'https://builtwith.appwrite.io',
   });
 };
