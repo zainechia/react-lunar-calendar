24abddf1cc8fb486cd7febefcec61db81fb89732
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-14 11:38:43 +0700

    Update main.js

--- updateOneSignalUsers/src/main.js
+++ updateOneSignalUsers/src/main.js
@@ -10,7 +10,7 @@ export default async ({ req, res, log, error }) => {
   }
 
   if (req.method === 'POST') {
-    log(req.body.event);
+    log(req.body);
   }
   //delete one signal user function
 
