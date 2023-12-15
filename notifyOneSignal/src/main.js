368a9f450eaf12ebe277ec9b21f5a5d9904ed651
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-15 17:21:36 +0700

    Update main.js

--- notifyOneSignal/src/main.js
+++ notifyOneSignal/src/main.js
@@ -64,10 +64,10 @@ export default async ({ req, res, log, error }) => {
   // }
 
   // `res.json()` is a handy helper for sending JSON
-  // return res.json({
-  //   motto: 'Build like a team of hundreds_',
-  //   learn: 'https://appwrite.io/docs',
-  //   connect: 'https://appwrite.io/discord',
-  //   getInspired: 'https://builtwith.appwrite.io',
-  // });
+  return res.json({
+    motto: 'Build like a team of hundreds_',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
+  });
 };
