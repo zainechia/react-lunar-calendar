b4fac29b9a2be8212d98c86a21c2c883a29893ad
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-15 17:57:11 +0700

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,12 +3,52 @@ import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import EventCalendar from "./EventCalendar";
 import { Stack } from "@mui/material";
-import { databases, account, COLLECTION_ID } from "../appwrite/appwriteConfig";
+import {
+  databases,
+  account,
+  COLLECTION_ID,
+  DATABASE_ID,
+} from "../appwrite/appwriteConfig";
 import { Query } from "appwrite";
 import NavBar from "./NavBar";
 import Login from "./Login";
 import { Snackbar, Alert } from "@mui/material";
 import OneSignal from "react-onesignal";
+
+// Check for events happening within the next hour in Appwrite
+const checkEventsWithinNextHour = async (
+  databases,
+  databaseId,
+  collectionId
+) => {
+  try {
+    // Get the current time
+    const currentTime = new Date();
+
+    // Calculate the time one hour from now
+    const nextHour = new Date(currentTime.getTime() + 60 * 60 * 1000);
+
+    // Query for events starting within the next hour
+    const query = [
+      Query.greaterThan("startTime", currentTime.toISOString()), // Events starting after the current time
+      Query.lessThan("startTime", nextHour.toISOString()), // Events starting before the next hour
+    ];
+    console.log(OneSignal.Client);
+    console.log("currentTime", currentTime.toISOString());
+    console.log("nextHour", nextHour.toISOString());
+    console.log("query", query);
+
+    const documents = await databases.listDocuments(
+      databaseId,
+      collectionId,
+      query
+    );
+    console.log("documents", documents);
+    return documents;
+  } catch (error) {
+    throw error;
+  }
+};
 
 function Profile() {
   const navigate = useNavigate();
@@ -27,7 +67,26 @@ function Profile() {
         }
       );
     }
-    
+    try {
+      // Use the helper function to check events within the next hour
+      const eventsWithinNextHour = checkEventsWithinNextHour(
+        databases,
+        DATABASE_ID,
+        COLLECTION_ID
+      );
+
+      // Do something with eventsWithinNextHour
+      // Iterate over eventsWithinNextHour
+      // Find userID for that event
+      // Use sendOneSignalNotification helper function to notify userID - create notification using filters for userID data tag
+      // Keep count of number of times notification sent, make sure only send once
+      // Cronjob Appwrite function will run every minute
+
+      log("Events within the next hour:", eventsWithinNextHour);
+    } catch (error) {
+      error("Error checking events within the next hour:", error.message);
+    }
+
     return () => (effectRan.current = true);
   }, []);
 
