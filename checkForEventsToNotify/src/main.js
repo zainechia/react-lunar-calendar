809d37636d8aa83a821c3544dd173852ceb2ec3b
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-15 16:34:06 +0700

    move all functions in to main.js

--- checkForEventsToNotify/src/main.js
+++ checkForEventsToNotify/src/main.js
@@ -1,10 +1,31 @@
-import { checkEventsWithinNextHour, sendOneSignalNotification } from "./helperFunctions";
 import {
     databases,
     DATABASE_ID,
     COLLECTION_ID,
   } from "../../src/appwrite/appwriteConfig";
+import { Query } from "appwrite";
 
+  // Check for events happening within the next hour in Appwrite
+  const checkEventsWithinNextHour = async (databases, databaseId, collectionId) => {
+    try {
+      // Get the current time
+      const currentTime = new Date();
+  
+      // Calculate the time one hour from now
+      const nextHour = new Date(currentTime.getTime() + 60 * 60 * 1000);
+  
+      // Query for events starting within the next hour
+      const query = [
+        Query.greaterThan("startTime", currentTime.toISOString()), // Events starting after the current time
+        Query.lessThan("startTime", nextHour.toISOString()), // Events starting before the next hour
+      ];
+  
+      const { documents } = await databases.listDocuments(databaseId, collectionId, query);
+      return documents;
+    } catch (error) {
+      throw error;
+    }
+  };
   
 // This is your main Appwrite function
 export default async ({ req, res, log, error }) => {
