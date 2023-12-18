d0652f58379f68d6ae8f42899b62760526b13856
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-18 15:38:58 +0700

    Update main.js

--- notifyOneSignal/src/main.js
+++ notifyOneSignal/src/main.js
@@ -1,8 +1,13 @@
-import { Query, Client, Databases } from 'node-appwrite';
-import * as OneSignal from '@onesignal/node-onesignal';
+import { Query, Client, Databases } from "node-appwrite";
+import * as OneSignal from "@onesignal/node-onesignal";
 
 // Helper function: Check for events happening within the next hour in Appwrite
-const getEventsWithinNextHour = async (log, database, databaseId, collectionId) => {
+const getEventsWithinNextHour = async (
+  error,
+  database,
+  databaseId,
+  collectionId
+) => {
   try {
     // Get the current time
     const currentTime = new Date();
@@ -16,33 +21,43 @@ const getEventsWithinNextHour = async (log, database, databaseId, collectionId) 
       Query.lessThanEqual("startTime", nextHour.toISOString()), // Events starting before the next hour
     ];
 
-    const documents = await database.listDocuments(databaseId, collectionId, query);
+    const documents = await database.listDocuments(
+      databaseId,
+      collectionId,
+      query
+    );
 
     return documents;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    error("Error getting events happening within an hour:", err.message);
+    throw err;
   }
 };
 
 // Send One Signal notification
-const sendOneSignalNotification = async (oneSignalClient, oneSignalAppId, notificationContent) => {
+const sendOneSignalNotification = async (
+  error,
+  oneSignalClient,
+  oneSignalAppId,
+  notificationContent
+) => {
   try {
     const notification = new OneSignal.Notification();
     notification.app_id = oneSignalAppId;
 
     notification.contents = {
-      en: notificationContent
-    }
+      en: notificationContent,
+    };
 
     notification.included_segments = ["All"];
 
-    log('Sending OneSignal notification...');
+    log("Sending OneSignal notification...");
     const result = await oneSignalClient.createNotification(notification);
-    log('OneSignal notification sent successfully:', result);
+    log("OneSignal notification sent successfully:", result);
     return result;
-  } catch (error) {
-    error('Error sending OneSignal notification:', error.message);
-    throw(error);
+  } catch (err) {
+    error("Error sending OneSignal notification:", err.message);
+    throw err;
   }
 };
 
@@ -52,16 +67,17 @@ export default async ({ req, res, log, error }) => {
   // Appwrite constants
   const DATABASE_ID = "656fd0d5e096d5c69451";
   const COLLECTION_ID = "656fd11d07243c7e0ea1";
-  
+
   // OneSignal constants
   const ONE_SIGNAL_APP_ID = "8c92528a-fc7e-4083-b4c9-2b5c21c1b2d8";
-  const ONE_SIGNAL_USER_KEY = "YzNhYTIzMjgtMTE3Yy00ZGU3LWJlOWEtZjEwNzEyNjVjNThi";
+  const ONE_SIGNAL_USER_KEY =
+    "YzNhYTIzMjgtMTE3Yy00ZGU3LWJlOWEtZjEwNzEyNjVjNThi";
 
   // Initialize Appwrite client and database
   const appwriteClient = new Client()
-     .setEndpoint('https://cloud.appwrite.io/v1')
-     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-     .setKey(process.env.APPWRITE_API_KEY);
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
   const database = new Databases(appwriteClient);
 
   // Initialize OneSignal client
@@ -75,18 +91,22 @@ export default async ({ req, res, log, error }) => {
   let notificationContent = "Hello World!";
 
   // Send notification to all users
-  sendOneSignalNotification(oneSignalClient, ONE_SIGNAL_APP_ID, notificationContent);
+  sendOneSignalNotification(
+    oneSignalClient,
+    ONE_SIGNAL_APP_ID,
+    notificationContent
+  );
 
   // You can log messages to the console
-  log('Hello, Logs!');
+  log("Hello, Logs!");
 
   // If something goes wrong, log an error
-  error('Hello, Errors!');
+  error("Hello, Errors!");
 
   try {
     // Use the helper function to check events within the next hour
     const eventsWithinNextHour = await getEventsWithinNextHour(
-      log,
+      error,
       database,
       DATABASE_ID,
       COLLECTION_ID
@@ -94,24 +114,23 @@ export default async ({ req, res, log, error }) => {
 
     log("Events within the next hour:");
     log(eventsWithinNextHour);
-    
+
     // Do something with eventsWithinNextHour
     // Iterate over eventsWithinNextHour
     // Find userID for that event
     // Use sendOneSignalNotification helper function to notify userID - create notification using filters for userID data tag
     // Keep count of number of times notification sent, make sure only send once
     // Cronjob Appwrite function will run every minute
-
-  } catch (error) {
-    error("Error checking events within the next hour:", error.message);
-    throw(error);
+  } catch (err) {
+    error("Error with Appwrite function:", err.message);
+    throw err;
   }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
   });
 };
