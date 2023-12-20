e8aaa973570f04ea7fc58d5e354580793258e28e
zainechia <74528254+zainechia@users.noreply.github.com>
2023-12-20 17:37:22 +0700

    Update PublicRoute.js

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 function PublicRoute({ children }) {
   const isAuthenticated = localStorage.getItem("session") ? true : false;
 
-  return !isAuthenticated ? <>{children}</> : <Navigate to={"profile"} />;
+  return !isAuthenticated ? <>{children}</> : <Navigate to={"/profile"} />;
 }
 
 export default PublicRoute;
